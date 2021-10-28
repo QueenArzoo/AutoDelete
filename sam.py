e9d88fd1feb78bd499341dfa14bcd3f9cfd37ac3
QueenArzoo <64604049+QueenArzoo@users.noreply.github.com>
2021-10-29 06:48:12 +0800

    Update sam.py

--- sam.py
+++ sam.py
@@ -58,11 +58,11 @@ def _start(client, message):
             return
         except Exception:
             client.send_message(message.chat.id,
-                text="**👋🏻 Hey [{}](tg://user?id={})**\n__This bot you can protect your channel and lock and open media** \nn__Add the bot admin in your channel directly**".format(message.from_user.first_name, message.from_user.id),
+                text="**👋🏻 Hey [{}](tg://user?id={})**\n__This bot you can protect your channel and lock and open media** \n__Add the bot admin in your channel directly**".format(message.from_user.first_name, message.from_user.id),
 	        reply_markup=InlineKeyboardMarkup(
                     [
                         [   
-                           InlineKeyboardButton("Join Updates Channel", url="https://t.me/LaylaList"),
+                           InlineKeyboardButton("Updates Channel", url="https://t.me/LaylaList"),
                            InlineKeyboardButton("Support Group", url="https://t.me/AwesomeSupport")
                       ],
                      [
