d03a3a3a1a119bb0f5d8dee7c79f55f28f6c9597
QueenArzoo <64604049+QueenArzoo@users.noreply.github.com>
2021-10-29 06:12:26 +0800

    Update info.py

--- info.py
+++ info.py
@@ -4,6 +4,7 @@ from os import environ
 API_ID = int(os.environ.get("API_ID", 12345))
 API_HASH = os.environ.get("API_HASH", None)
 SESSION = os.environ.get("SESSION")
+BOT_USERNAME = os.environ.get("BOT_USERNAME")
 TOKEN = os.environ.get("TOKEN")
 ADMINS = set(int(x) for x in os.environ.get("ADMINS", "1914868167").split())
 TIME = os.environ.get("TIME", None)
