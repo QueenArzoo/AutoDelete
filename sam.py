f92b74035afdfed89574d2795d1d9991296eccaa
QueenArzoo <64604049+QueenArzoo@users.noreply.github.com>
2021-10-29 02:49:11 +0800

    Update sam.py

--- sam.py
+++ sam.py
@@ -31,7 +31,6 @@ Sam = Client(
 
 @Sam.on_message(filters.group & filters.all)
 async def deleter(bot: Client, cmd: Message):
-         if cmd.from_user.id not in ADMINS:
                   await slp(int(TIME))
                   await cmd.delete()
 
